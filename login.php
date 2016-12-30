e1fd6d3e251b70e9c7cd8ebe4f36f2fdc210f44a
s1t3 <letritue1589@gmail.com>
2016-12-30 14:36:35 -0500

    Add files via upload

---
+++ login.php
@@ -0,0 +1,27 @@
+<html>
+
+<style> 
+body{text-align: center;
+	color: green;
+	font-size:30px;
+
+}
+
+
+</style>
+<body>
+
+Welcome 
+<br>
+<?php echo $_POST["username"]; ?>
+
+
+ 	
+
+<!--
+Your email address is: <?php echo $_POST[""]; ?>
+
+-->
+
+</body>
+</html>
